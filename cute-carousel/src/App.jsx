7ee73bf8b6c089c61a14a68ad8a6c3be32c6ebbe
Tuğçe Karakuş <karakustugce1453@gmail.com>
2026-06-24 16:43:23 +0300

    feat(cute-carousel): add next/previous navigation to animal carousel

--- cute-carousel/src/App.jsx
+++ cute-carousel/src/App.jsx
@@ -1,17 +1,36 @@
 import './style.css'
 import { animals } from '../data'
+import { useState } from 'react'
 function App() {
-  const animal=animals[5]
+  const [index,setIndex]=useState(0)
+  const animal=animals[index]
+  
+  function handleNextClick() {
+    if(index<animals.length-1){
+      setIndex(index+1)
+    }
+    else{
+      setIndex(0)
+    }
+  }
+  function handlePreviousClick() {
+    if(index>0){
+      setIndex(index-1)
+    }
+    else{
+      setIndex(animals.length-1)
+    }
+  }
   return (
     <>
      <h1 className='text-5xl text-center mb-5 text-dark-pink'>Cute Carousel</h1>
-     <div className="container bg-bg py-5 px-4 rounded-4xl space-y-3 max-w-md">
-      <p>1 of 6</p>
-      <img src={animal.image} className='rounded-2xl ' alt=""/>
+     <div className="container bg-bg py-5 px-4 rounded-4xl space-y-3 max-w-sm  flex flex-col">
+      <p>{index+1} of {animals.length-1}</p>
+      <img src={animal.image} className='rounded-2xl' alt=""/>
       <p className='text-center'>{animal.description}</p>
       <div className='flex justify-center gap-10'>
-        <button className='bg-button rounded-full py-3 px-7'>Previous</button>
-        <button className='bg-button rounded-full py-3 px-7'>Next</button>
+        <button className='bg-button rounded-full py-3 px-7' onClick={handlePreviousClick}>Previous</button>
+        <button className='bg-button rounded-full py-3 px-7' onClick={handleNextClick}>Next</button>
       </div>
      </div>
     </>
